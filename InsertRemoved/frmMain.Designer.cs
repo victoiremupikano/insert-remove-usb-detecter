268ebce2f0a4f834f2591f9be8cb22197d4c15e2
victoirempkn <96738243+victoiremupikano@users.noreply.github.com>
2023-09-17 06:28:24 +0200

    suppression du test de la detection des smartphones

--- InsertRemoved/frmMain.Designer.cs
+++ InsertRemoved/frmMain.Designer.cs
@@ -30,27 +30,53 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
             this.lstDevices = new System.Windows.Forms.ListBox();
+            this.tabControlMain = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabControlMain.SuspendLayout();
+            this.tabPage1.SuspendLayout();
             this.SuspendLayout();
             // 
             // lstDevices
             // 
             this.lstDevices.Dock = System.Windows.Forms.DockStyle.Fill;
             this.lstDevices.FormattingEnabled = true;
-            this.lstDevices.Location = new System.Drawing.Point(0, 0);
+            this.lstDevices.Location = new System.Drawing.Point(3, 3);
             this.lstDevices.Name = "lstDevices";
-            this.lstDevices.Size = new System.Drawing.Size(605, 367);
+            this.lstDevices.Size = new System.Drawing.Size(591, 335);
             this.lstDevices.TabIndex = 0;
+            // 
+            // tabControlMain
+            // 
+            this.tabControlMain.Controls.Add(this.tabPage1);
+            this.tabControlMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControlMain.Location = new System.Drawing.Point(0, 0);
+            this.tabControlMain.Name = "tabControlMain";
+            this.tabControlMain.SelectedIndex = 0;
+            this.tabControlMain.Size = new System.Drawing.Size(605, 367);
+            this.tabControlMain.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.lstDevices);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(597, 341);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "USB Classique";
+            this.tabPage1.UseVisualStyleBackColor = true;
             // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(605, 367);
-            this.Controls.Add(this.lstDevices);
+            this.Controls.Add(this.tabControlMain);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "frmMain";
             this.Text = "Inserer, detacher ...";
-            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.tabControlMain.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -58,6 +84,8 @@
         #endregion
 
         private System.Windows.Forms.ListBox lstDevices;
+        private System.Windows.Forms.TabControl tabControlMain;
+        private System.Windows.Forms.TabPage tabPage1;
     }
 }
 
